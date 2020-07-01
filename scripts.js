62b8a08586e627a51617e49def6595b3c46f84b2
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-07-01 17:49:45 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Manu");
+$("#policyNumber").text("PMY6209647");
+$("#startDate").text("02-Jul-2020");
+$("#endDate").text("01-Jul-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08DC0931");
+$("#make").text("HONDA");
+$("#model").text("ACTIVA");
+$("#vehicalSubType").text("3G");
+$("#engineNo").text("JF50ET2509686");
+$("#chassis").text("ME4JF504GFT508623");
